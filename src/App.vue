2a486bf6a97eb9aa6da3610ed56b802cba76d0ad
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-02-28 17:14:17 +0100

    Prima App Vite creata

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <script>
+    import AppTitle from "./components/AppTitle.vue";
 
+    export default {
+        components: {
+            AppTitle,
+        }
+    }
 </script>
 
 <template>
-  
+    <AppTitle></AppTitle>
 </template>
 
 <style>
-
+    
 </style>
